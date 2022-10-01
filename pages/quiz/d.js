6934ee48c7e3addc78aec55b80f283afb00280c5
Alabi Damilola Daniel <eminencemailx@gmail.com>
2022-10-01 07:20:25 -0700

    quiz page created

--- pages/quiz/d.js
+++ pages/quiz/d.js
@@ -1,20 +1,45 @@
+import React from "react"
+import Header from "../../components/Header"
+import styles from '../../styles/Quiz.module.css'
 
 
-const quiz = ()=>{
+const Quiz = ()=>{
     return(
         <div className={styles.container}>
+            <Header/>
+            <div className={styles.hbackgroud}></div>
              <div className={styles.quizSection}>
                  <div className={styles.leftCon}>
-                       
+                        Scale up!!
                  </div>
                  <div className={styles.rightCon}>
                            <h1>QUIZ</h1>
                            <hr/>
+                           <p>this is the question answer answer?</p>
+                           <hr/>
                            <div className={styles.optionCon}>
-                            
+                          <div className={styles.rightOpt}>
+                              <div className={styles.option}>
+                                  answer 1
+                              </div>
+                              <div className={styles.option}>
+                                  answer 1
+                              </div>
+                           </div>
+                           <div className={styles.leftOpt}>
+                                
+                              <div className={styles.option}>
+                                  answer 1
+                              </div><div className={styles.option}>
+                                  answer 1
+                              </div>
+                           </div>
                            </div>
                  </div>
              </div>
+             
         </div>
     )
 }
+
+export default Quiz

02d11414755b09cb00710f4f364e97637451d8fe
Estherfisayomi12 <fisayomi2602@gmail.com>
2022-10-01 15:14:18 +0100

    Styled Login Page

--- pages/login.js
+++ pages/login.js
@@ -1,47 +1,46 @@
-
-import Header from "../components/Header"
-import styles from '../styles/Login.module.css'
+import Header from "../components/Header";
+import styles from "../styles/Login.module.css";
 import Image from "next/image";
 import Footer from "../components/Footer";
 
 import Link from "next/link";
 import Menu from "../components/Menu";
 
+const Login = () => {
+  return (
+    <div className={styles.container}>
+      <Header />
 
-
-const Login =()=>{
-    return(
-        <div  className={styles.container}>
-           <Header/>
-          
-           <div className={styles.hbackgroud}></div>
-          <div className={styles.loginCon}>
-
-           <div className={styles.leftCon}>
-                 <h1>Welcome Back Scholar</h1>
-                 <p>kindly input your email and password, login to continue learning on astroMutatio</p>
-
-           </div>
-           <div className={styles.rightCon}>
-
-               <form >
-                <div className={styles.field}>
-                    <label htmlFor="email" >Email</label>
-                    <input type="email" id="email" placeholder="Email address...."/>
-                </div>
-                <div className={styles.field}>
-                    <label htmlFor="password" >Password</label>
-                    <input type="password" id="password" placeholder="Enter password...."/>
-                </div>
-               </form>
-            
-          </div>
-          </div>
-          
-           <Menu/>
-            <Footer/>
+      <div className={styles.hbackgroud}></div>
+      <div className={styles.loginCon}>
+        <div className={styles.leftCon}>
+          <h1>Welcome Back Scholar</h1>
+          <p>
+            kindly input your email and password, login to continue learning on
+            astroMutatio
+          </p>
         </div>
-    )
-}
+        <div className={styles.rightCon}>
+          <form>
+            <div className={styles.field}>
+              <input type="email" id="email" placeholder="Email Address" />
+            </div>
+            <div className={styles.field}>
+              <input type="password" id="password" placeholder="Password" />
+            </div>
+            <button className={styles.button}>Log In</button>
+            <a href="#" className={styles.link}>
+              Forgotten Password?
+            </a>
+            <button className={styles.button}>Create New Account</button>
+          </form>
+        </div>
+      </div>
 
-export default Login
+      <Menu />
+      <Footer />
+    </div>
+  );
+};
+
+export default Login;
